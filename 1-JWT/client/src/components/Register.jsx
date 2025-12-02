6a3a8574983e1a0881ca91ebe3f4bf722fb8520c
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2025-12-02 13:48:07 +0530

    Implemented Register and Verify OTP UI

---
+++ 1-JWT/client/src/components/Register.jsx
@@ -0,0 +1,84 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+function Register() {
+  const navigateTo = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const handleRegister = async (data) => {
+    data.phone = `+91${data.phone}`;
+    await axios
+      .post("http://localhost:8000/api/v1/user/register", data, {
+        withCredentials: true,
+        headers: { "Content-Type": "application/json" },
+      })
+      .then((res) => {
+        toast.success(res.data.message);
+        navigateTo(`/otp-verification/${data.email}/${data.phone}`);
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
+  };
+  return (
+    <>
+      <form className="auth-form" onSubmit={handleSubmit(handleRegister)}>
+        <h2>Register</h2>
+        <input type="text" placeholder="Name" required {...register("name")} />
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          {...register("email")}
+        />
+        <div>
+          <span>+91</span>
+          <input
+            type="number"
+            placeholder="Phone"
+            required
+            {...register("phone")}
+          />
+        </div>
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          {...register("password")}
+        />
+        <div className="verification-method">
+          <p>Select Verification Method</p>
+          <div className="wrapper">
+            <label>
+              <input
+                type="radio"
+                name="verificationMethod"
+                value={"email"}
+                required
+                {...register("verificationMethod")}
+              />
+              Email
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="verificationMethod"
+                value={"phone"}
+                required
+                {...register("verificationMethod")}
+              />
+              Phone
+            </label>
+          </div>
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+}
+
+export default Register;
