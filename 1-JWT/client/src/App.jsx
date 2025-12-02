6fab4f8dc98472c937124b1568e32c2863728b9b
Nikhil Yadav <nikhilyadav.prof@gmail.com>
2025-12-02 13:45:38 +0530

    Implemented Authentication UI

--- 1-JWT/client/src/App.jsx
+++ 1-JWT/client/src/App.jsx
@@ -1,7 +1,33 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import OtpVerification from "./pages/OtpVerification";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/otp-verification/:email/:phone"
+            element={<OtpVerification />}
+          />
+          <Route path="/password/forgot" element={<ForgotPassword />} />
+          <Route path="/password/reset/:token" element={<ResetPassword />} />
+        </Routes>
+        <ToastContainer theme="colored" />
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- 1-JWT/client/src/pages/Auth.jsx
+++ 1-JWT/client/src/pages/Auth.jsx
@@ -1,5 +1,42 @@
+import { useContext, useState } from "react";
+import { Context } from "../main";
+import { Navigate } from "react-router-dom";
+import "../styles/Auth.css";
+import Login from "../components/Login";
+import Register from "../components/Register";
 function Auth() {
-  return <></>;
+  const { isAuthenticated } = useContext(Context);
+  const [isLogin, setIsLogin] = useState(true);
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <>
+      <div className="auth-page">
+        <div className="auth-container">
+          <div className="auth-toggle">
+            <button
+              className={`toggle-btn ${isLogin ? "active" : ""}`}
+              onClick={() => {
+                setIsLogin(true);
+              }}
+            >
+              Login
+            </button>
+            <button
+              className={`toggle-btn ${!isLogin ? "active" : ""}`}
+              onClick={() => {
+                setIsLogin(false);
+              }}
+            >
+              Register
+            </button>
+          </div>
+          {isLogin ? <Login /> : <Register />}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Auth;
